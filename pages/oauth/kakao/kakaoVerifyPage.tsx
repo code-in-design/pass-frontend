f0ad9873efd9a0b45de34d4d5cc5d6f9ebe77bad
seonsubin <stnqls3938@gmail.com>
2023-05-23 16:08:39 +0900

    폴더명 변경

---
+++ pages/oauth/kakao/kakaoVerifyPage.tsx
@@ -0,0 +1,7 @@
+import KakaoVerify from '@/features/auth/components/OAuth/KakaoVerify';
+
+function kakaoVerifyPage() {
+  return <KakaoVerify />;
+}
+
+export default kakaoVerifyPage;
---
+++ pages/oauth/naver/naverVerifyPage.tsx
@@ -0,0 +1,6 @@
+import NaverVerify from '@/features/auth/components/OAuth/NaverVerify';
+import React from 'react';
+
+export default function naverVerifyPage() {
+  return <NaverVerify />;
+}
