e0cabe9de29da87db06514359ad10102039c5789
seonsubin <stnqls3938@gmail.com>
2023-05-25 09:28:50 +0900

    파일명변경

--- pages/oauth/kakao/kakaoVerifyPage.tsx
+++
@@ -1,9 +0,0 @@
-import BarLoaderSpinner from '@/components/Spinners/BarLoaderSpinner';
-import { useFetchKakaoVerifyQuery } from '@/features/auth/apis/authApi';
-
-function KakaoVerifyPage() {
-  useFetchKakaoVerifyQuery();
-  return <BarLoaderSpinner color="#6B77F8" />;
-}
-
-export default KakaoVerifyPage;
--- pages/oauth/naver/naverVerifyPage.tsx
+++
@@ -1,9 +0,0 @@
-import { useFetchNaverVerifyQuery } from '@/features/auth/apis/authApi';
-import BarLoaderSpinner from '@/components/Spinners/BarLoaderSpinner';
-import { useEffect } from 'react';
-
-export default function NaverVerifyPage() {
-  useFetchNaverVerifyQuery();
-
-  return <BarLoaderSpinner color="#6B77F8" />;
-}
