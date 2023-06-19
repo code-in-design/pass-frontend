6185c63db758fbf046a76d726e375a93735c0ed7
seonsubin <stnqls3938@gmail.com>
2023-06-19 16:56:48 +0900

    오류해결

--- src/features/passAnalysis/container/ExpectedAverageScoresContainer.tsx
+++ src/features/passAnalysis/container/ExpectedAverageScoresContainer.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import ExpectedAverageScores from '../components/ExpectedAverageScores';
 
 const ExpectedAverageScoresContainer = () => {
-  const { register, setValue } = useForm();
-  return <ExpectedAverageScores register={register} setValue={setValue} />;
+  return <ExpectedAverageScores />;
 };
 
 export default ExpectedAverageScoresContainer;
