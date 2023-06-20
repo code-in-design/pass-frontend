431d9ac7c52e1c0add6bf70e99d40ba32df979fa
seonsubin <stnqls3938@gmail.com>
2023-06-20 16:50:59 +0900

    실기기록 입력 form

--- src/features/passAnalysis/components/PassAnalysisMenu/PracticalChangeScoreForm.tsx
+++ src/features/passAnalysis/components/PassAnalysisMenu/PracticalChangeScoreForm.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Checkbox, Radio, RadioGroup, Stack } from '@chakra-ui/react';
+import { Radio, RadioGroup, Stack } from '@chakra-ui/react';
 import { exerciseType } from '@/components/PracticalIcon/PracticalType';
 import ExclamationMark from '../../../../../public/images/icons/exclamation.svg';
+import { FieldValues, UseFormRegister, UseFormSetValue } from 'react-hook-form';
 
 interface Props {
   type: string;
   index?: number;
+  practicalScore?: string[];
   lastType: number;
   goPrevStep: () => void;
   goNextStep: () => void;
+  register: UseFormRegister<FieldValues>;
+  setValue: UseFormSetValue<FieldValues>;
 }
 
 const PracticalChangeScoreForm = (props: Props) => {
@@ -18,44 +22,42 @@ const PracticalChangeScoreForm = (props: Props) => {
   return (
     <Container>
       <FormContainer>
-        <Title>실기 기록 입력 확인</Title>
+        <Title>실기 기록 입력</Title>
         <Information>
           <InfoIconWrapper>
             <ExclamationMark />
           </InfoIconWrapper>
           기록 변경 횟수가 제한되어 있으니 신중히 입력하세요!
         </Information>
-        {/* 직접 입력 */}
         <PracticalName>
           <ExerciseIconWrapper>{exerciseIcon.icon}</ExerciseIconWrapper>
           {exerciseIcon.text}
         </PracticalName>
-        <Content>
-          <InputWrapper>
-            <Input />
-            <MetricUnits>cm</MetricUnits>
-          </InputWrapper>
-        </Content>
-        {/* 객관식 */}
-        <PracticalName>
-          <ExerciseIconWrapper>{exerciseIcon.icon}</ExerciseIconWrapper>
-          {exerciseIcon.text}
-        </PracticalName>
-        <Content>
-          <RadioGroup>
-            <Stack direction="column">
-              <Radio value="1" variant="outline">
-                3초 이상 유지 후 앞구르기 깔끔하게 수행
-              </Radio>
-              <Radio value="2" variant="outline">
-                1~3초 무난하게 유지 후 앞구르기 깔끔하게 수행
-              </Radio>
-              <Radio value="3" variant="outline">
-                다소 아쉬운 물구나무서기 후 무난한 앞구르기 수행
-              </Radio>
-            </Stack>
-          </RadioGroup>
-        </Content>
+        {props?.practicalScore ? (
+          // 객관식 입력
+          <Content>
+            <RadioGroup>
+              <Stack direction="column">
+                {props?.practicalScore.map((item, index) => {
+                  return (
+                    <Radio {...props.register(exerciseIcon.text)} key={index} value={String(index)} variant="outline">
+                      {item}
+                    </Radio>
+                  );
+                })}
+              </Stack>
+            </RadioGroup>
+          </Content>
+        ) : (
+          // 주관식 입력
+          <Content>
+            <InputWrapper>
+              <Input {...props.register(exerciseIcon.text)} />
+              <MetricUnits>cm</MetricUnits>
+            </InputWrapper>
+          </Content>
+        )}
+
         <Buttons>
           <Button type="button" onClick={props.goPrevStep}>
             이전
--- src/features/passAnalysis/container/PracticalChangeScoreContainer.tsx
+++ src/features/passAnalysis/container/PracticalChangeScoreContainer.tsx
@@ -1,32 +1,66 @@
 import React, { useCallback, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import PracticalChangeScoreForm from '../components/PassAnalysisMenu/PracticalChangeScoreForm';
 
 interface Props {
-  types: string[];
+  types: {
+    name: string;
+    multipleChoice?: string[];
+  }[];
 }
 
 const PracticalChangeScoreContainer = (props: Props) => {
-  let index = 0;
-  const [type, setType] = useState(props.types[0]);
+  let step = 0;
+  const [practicalName, setPracticalName] = useState(props.types[0].name);
+  const [practicalScore, setPracticalScore] = useState(props.types[0]?.multipleChoice);
+  const { register, handleSubmit, setValue, getValues } = useForm();
 
   const goPrevStep = useCallback(() => {
-    index = index - 1;
-    setType(props.types[index]);
+    step = step - 1;
+    setPracticalName(props.types[step].name);
+    setPracticalScore(props.types[step]?.multipleChoice);
   }, []);
 
   const goNextStep = useCallback(() => {
-    index = index + 1;
-    setType(props.types[index]);
+    step = step + 1;
+    setPracticalName(props.types[step].name);
+    setPracticalScore(props.types[step]?.multipleChoice);
   }, []);
 
+  const onsubmit = data => {
+    console.log(data);
+  };
+
   return (
-    <form action="">
-      <PracticalChangeScoreForm type={type} goPrevStep={goPrevStep} goNextStep={goNextStep} index={index} lastType={props.types.length} />
+    <form onSubmit={handleSubmit(onsubmit)}>
+      <PracticalChangeScoreForm type={practicalName} practicalScore={practicalScore} register={register} setValue={setValue} goPrevStep={goPrevStep} goNextStep={goNextStep} index={step} lastType={props.types.length} />
     </form>
   );
 };
 
 PracticalChangeScoreContainer.defaultProps = {
-  types: ['제자리 멀리뛰기', '배근력', '사이드스텝', '서전트점프', '매달리기', '메디신볼던지기'],
+  types: [
+    {
+      name: '제자리 멀리뛰기',
+      multipleChoice: ['3초 이상 유지 후 멀리뛰기 깔끔하게 수행', '1~3초 무난하게 유지 후 멀리뛰기 깔끔하게 수행', '다소 아쉬운 물구나무서기 후 무난한 멀리뛰기 수행'],
+    },
+    {
+      name: '배근력',
+      multipleChoice: ['3초 이상 유지 후 배근력 깔끔하게 수행', '1~3초 무난하게 유지 후 배근력 깔끔하게 수행', '다소 아쉬운 물구나무서기 후 무난한 배근력 수행'],
+    },
+    {
+      name: '사이드스텝',
+    },
+    {
+      name: '서전트점프',
+    },
+    {
+      name: '매달리기',
+    },
+    {
+      name: '메디신볼던지기',
+      multipleChoice: ['3초 이상 유지 후 볼던지기 깔끔하게 수행', '1~3초 무난하게 유지 후 볼던지기 깔끔하게 수행', '다소 아쉬운 물구나무서기 후 무난한 볼던지기 수행'],
+    },
+  ],
 };
 export default PracticalChangeScoreContainer;
