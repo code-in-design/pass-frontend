b65823769b0b880d8fb98c7157feac3757e75742
seonsubin <stnqls3938@gmail.com>
2023-06-29 16:34:07 +0900

    멤버십table수정

--- src/components/Modal/MembershipModal/MembershipPriceTable.tsx
+++ src/components/Modal/MembershipModal/MembershipPriceTable.tsx
@@ -1,96 +1,147 @@
+import { ReactNode, useMemo } from 'react';
+import { useTable } from 'react-table';
 import styled from '@emotion/styled';
-import TableItem from './MembershipPriceTableItem';
 import Circle from '../../../../public/images/icons/circle.svg';
 import X from '../../../../public/images/icons/X.svg';
-import { ReactNode } from 'react';
 
 interface TableProps {
   title: string;
-  items: string | ReactNode[];
+  Basic: string | ReactNode;
+  Light: string | ReactNode;
+  Premium: string | ReactNode;
 }
 
 interface Props {
   data: Array<TableProps>;
 }
 
-const MembershipPriceTable = ({ data }: Props) => {
+const MembershipPriceTable = (props: Props) => {
+  const data = props.data;
+
+  const columns = useMemo(
+    () => [
+      { Header: '', accessor: 'title' },
+      { Header: 'Basic', accessor: 'Basic', headerStyle: { color: '#9395A6' } },
+      { Header: 'Light', accessor: 'Light', headerStyle: { color: '#45BFD9' } },
+      { Header: 'Premium', accessor: 'Premium', headerStyle: { color: '#6B77F8' } },
+    ],
+    [],
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
   return (
-    <TableContainer>
+    <TableContainer {...getTableProps()}>
       <THead>
-        <THeadTr>
-          <THedaTd></THedaTd>
-          <THedaTd color="Basic">Basic</THedaTd>
-          <THedaTd color="Light">Light</THedaTd>
-          <THedaTd color="Premium">Premium</THedaTd>
-        </THeadTr>
-      </THead>
-      <TBody>
-        {data.map((item, index) => (
-          <TableItem key={index} title={item.title} items={item.items} />
+        {headerGroups.map(headerGroup => (
+          <THeadTr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <THedaTd {...column.getHeaderProps()} style={column.headerStyle}>
+                {column.render('Header')}
+              </THedaTd>
+            ))}
+          </THeadTr>
         ))}
+      </THead>
+      <TBody {...getTableBodyProps()}>
+        {rows.map(row => {
+          prepareRow(row);
+          return (
+            <TBodyTr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <TBodyTd {...cell.getCellProps()}>{cell.render('Cell')}</TBodyTd>;
+              })}
+            </TBodyTr>
+          );
+        })}
       </TBody>
     </TableContainer>
   );
 };
 
-export default MembershipPriceTable;
-
 MembershipPriceTable.defaultProps = {
   data: [
     {
       title: '실시간 인기 대학 확인',
-      items: [<Circle />, <Circle />, <Circle />],
+      Basic: <Circle />,
+      Light: <Circle />,
+      Premium: <Circle />,
     },
     {
       title: '모의고사 · 수능 성적 입력',
-      items: [<X />, <Circle />, <Circle />],
+      Basic: <X />,
+      Light: <Circle />,
+      Premium: <Circle />,
     },
     {
       title: '대학별 환산 점수 계산(실기 만점 기준)',
-      items: [<X />, <X />, <Circle />],
+      Basic: <X />,
+      Light: <X />,
+      Premium: <Circle />,
     },
     {
       title: '최종 실기 이후, 점수 공개 및 현재 등수 확인',
-      items: [<Circle />, <Circle />, <Circle />],
+      Basic: <Circle />,
+      Light: <Circle />,
+      Premium: <Circle />,
     },
     {
       title: '대학별 환산 총점 계산 (본인 수능,실기 기록,내신 기준)',
-      items: [<X />, '1개 대학·학과', <Circle />],
+      Basic: <X />,
+      Light: '1개 대학·학과',
+      Premium: <Circle />,
     },
     {
       title: 'Z-지수 자동 계산',
-      items: [<Circle />, <Circle />, <Circle />],
+      Basic: <Circle />,
+      Light: <Circle />,
+      Premium: <Circle />,
     },
     {
       title: '대학·학과 입시 정보 열람',
-      items: [<Circle />, <Circle />, <Circle />],
+      Basic: <Circle />,
+      Light: <Circle />,
+      Premium: <Circle />,
     },
     {
       title: '대학·학과 찾기 (세부 사항 필터링)',
-      items: [<Circle />, <Circle />, <Circle />],
+      Basic: <Circle />,
+      Light: <Circle />,
+      Premium: <Circle />,
     },
     {
       title: '관심 대학 설정',
-      items: [<X />, '1개 대학·학과', '최대 3개 대학/학과'],
+      Basic: <X />,
+      Light: '1개 대학·학과',
+      Premium: '최대 3개 대학/학과',
     },
     {
       title: '지원 가능성 분석(수능 점수 기준)',
-      items: ['전체 대학·학과', '전체 대학·학과', '전체 대학·학과'],
+      Basic: '전체 대학·학과',
+      Light: '전체 대학·학과',
+      Premium: '전체 대학·학과',
     },
     {
       title: '실기 기록 입력 및 수정',
-      items: [<X />, '3회', '3회'],
+      Basic: <X />,
+      Light: '3회',
+      Premium: '3회',
     },
     {
       title: '합격 확률 분석',
-      items: [<X />, '1개 대학·학과', '전체 대학·학과'],
+      Basic: <X />,
+      Light: '1개 대학·학과',
+      Premium: '전체 대학·학과',
     },
     {
       title: '예상 지원자 현황 열람',
-      items: [<X />, '1개 대학·학과', '전체 대학·학과'],
+      Basic: <X />,
+      Light: '1개 대학·학과',
+      Premium: '전체 대학·학과',
     },
   ],
 };
+
+export default MembershipPriceTable;
 
 const TableContainer = styled.table`
   width: 100%;
@@ -107,7 +158,6 @@ const THedaTd = styled.td`
   font-size: 16px;
   font-weight: 700;
   line-height: 20px;
-  color: ${props => (props.color === 'Basic' ? props.theme.colors.gray2 : props.color === 'Light' ? props.theme.colors.green : props.theme.colors.blue)};
   padding-bottom: 16px;
   width: 182px;
   text-align: center;
@@ -120,3 +170,27 @@ const THedaTd = styled.td`
   }
 `;
 const TBody = styled.tbody``;
+
+const TBodyTr = styled.tr`
+  border-bottom: 1px solid ${props => props.theme.colors.gray5};
+`;
+
+const TBodyTd = styled.td`
+  vertical-align: middle;
+  height: 40px;
+  font-size: 14px;
+  line-height: 32px;
+  font-weight: 600;
+  color: #6f6f6f;
+  text-align: center;
+  &:first-of-type {
+    padding-left: 16px;
+    text-align: left;
+  }
+  &:last-of-type {
+    padding-right: 53.5px;
+  }
+  & svg {
+    margin: 0 auto;
+  }
+`;
