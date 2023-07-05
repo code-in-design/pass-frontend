33c6afdda5c8871f799e2a478e5d3217dc5f7c46
dihoon <dihoon955@gmail.com>
2023-07-05 17:22:09 +0900

    테이블 피드백 반영

--- src/features/admin/AdminDashboard/components/Table.tsx
+++ src/features/admin/AdminDashboard/components/Table.tsx
@@ -1,182 +1,112 @@
 import { AgGridReact } from 'ag-grid-react';
 import { ColDef, GridOptions } from 'ag-grid-community';
-
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import styled from '@emotion/styled';
-import { useMemo } from 'react';
+import theme from '@/theme/theme';
 
-interface Props {
-  data: {
-    orderDate: string;
-    orderItems: number;
-    orderTotal: number;
-    NumberOfSamples: number;
-  }[];
+export interface TableProps<RowType> {
+  columns: ColDef[];
+  options: GridOptions;
+  rows: RowType[];
 }
 
-export const Table = (props: Props) => {
-  const { data } = props;
-  const orderItemsTotal = useMemo(() => data.reduce((sum, row) => sum + row.orderItems, 0), [data]);
-  const orderTotal = useMemo(() => data.reduce((sum, row) => sum + row.orderTotal, 0), [data]);
-  const NumberOfSamplesTotal = useMemo(() => data.reduce((sum, row) => sum + row.NumberOfSamples, 0), [data]);
-
-  const customizeCell = params => {
-    if (params.data && params.data.total) {
-      return { color: 'rgba(107, 119, 248, 1)', textAlign: 'center', fontFamily: 'Pretendard SemiBold', fontSize: 14 };
-    }
-    return { textAlign: 'center', fontFamily: 'Pretendard Regular', color: '', fontSize: '' };
-  };
-
-  const gridOptions: GridOptions = {
-    headerHeight: 36,
-    domLayout: 'autoHeight',
-    suppressMovableColumns: true,
-    pinnedBottomRowData: [
-      {
-        orderDate: '최근 7일 합계',
-        orderItems: orderItemsTotal,
-        orderTotal: orderTotal,
-        NumberOfSamples: NumberOfSamplesTotal,
-        total: true,
-      },
-    ],
-  };
-  const columnDefs: ColDef[] = [
-    { headerName: '일자', field: 'orderDate', flex: 1, minWidth: 96, cellStyle: { justifyContent: 'center', textAlign: 'center', fontFamily: 'Pretendard SemiBold' } },
-    { headerName: '주문수', field: 'orderItems', flex: 1, cellStyle: params => customizeCell(params) },
-    { headerName: '매출액', field: 'orderTotal', flex: 1, cellStyle: params => customizeCell(params), valueFormatter: params => params.value.toLocaleString() + '원' },
-    {
-      headerName: '유입표본수',
-      field: 'NumberOfSamples',
-      flex: 1,
-      minWidth: 96,
-      cellStyle: params => customizeCell(params),
-      valueFormatter: params => params.value.toLocaleString(),
-    },
-  ];
-
-  const getRowStyle = params => {
-    console.log(params);
-    if (params.data.total) {
-      return { fontSize: '14px' };
-    }
-    return {
-      padding: '',
-    };
-  };
-
+export const Table = <RowType,>(props: TableProps<RowType>) => {
+  const { rows, columns, options } = props;
   return (
     <AgiGridWrapper>
       <div className="ag-theme-alpine" style={{ height: '100%', width: '100%' }}>
-        <AgGridReact rowData={data} columnDefs={columnDefs} gridOptions={gridOptions} getRowHeight={() => 32}></AgGridReact>
+        <AgGridReact rowData={rows} columnDefs={columns} getRowHeight={() => 32} gridOptions={options}></AgGridReact>
       </div>
     </AgiGridWrapper>
   );
 };
 
-Table.defaultProps = {
-  data: [
-    {
-      orderDate: '2023-03-24',
-      orderItems: 3,
-      orderTotal: 300000,
-      NumberOfSamples: 1230,
-    },
-    {
-      orderDate: '2023-03-25',
-      orderItems: 3,
-      orderTotal: 300000,
-      NumberOfSamples: 1230,
-    },
-    {
-      orderDate: '2023-03-26',
-      orderItems: 3,
-      orderTotal: 300000,
-      NumberOfSamples: 1230,
-    },
-    {
-      orderDate: '2023-03-26',
-      orderItems: 3,
-      orderTotal: 300000,
-      NumberOfSamples: 1230,
-    },
-    {
-      orderDate: '2023-03-26',
-      orderItems: 3,
-      orderTotal: 300000,
-      NumberOfSamples: 1230,
-    },
-    {
-      orderDate: '2023-03-26',
-      orderItems: 3,
-      orderTotal: 300000,
-      NumberOfSamples: 1230,
-    },
-    {
-      orderDate: '2023-03-26',
-      orderItems: 3,
-      orderTotal: 300000,
-      NumberOfSamples: 1230,
-    },
-  ],
-};
-
 const AgiGridWrapper = styled.div`
   width: 100%;
 
-  .ag-root {
+  // 바디 고정 높이 제거
+  .ag-center-cols-clipper[role='presentation'] {
+    min-height: 0;
   }
 
-  .ag-header-row-column {
-    border-radius: 12px 12px 0px 0px;
-    background: var(--gray-6, #f8f8fc);
-  }
-
-  .ag-header {
-    border-bottom: 1px solid var(--gray-4, #e4e6f0);
-    line-height: 20px;
-    letter-spacing: -0.28px;
-  }
-
-  .ag-header-cell-label {
-    font-size: 14px;
-    font-family: Pretendard Bold;
+  // 로우 각 셀 패딩 제거, 가운데 정렬, 폰트 변경
+  .ag-row-even .ag-cell,
+  .ag-row-odd .ag-cell {
+    display: flex;
     justify-content: center;
-  }
-
-  .ag-cell {
+    align-items: center;
     padding: 0;
     font-size: 12px;
-  }
-
-  .ag-floating-bottom-container {
+    font-family: Pretendard Regular;
     line-height: 16px;
-    letter-spacing: -0.28px;
+    letter-spacing: -0.24px;
   }
 
-  .ag-floating-bottom-container .ag-cell[col-id='orderDate'] {
-    font-size: 14px;
-  }
-
+  // 로우 테두리 제거
   .ag-row-even,
   .ag-row-odd {
     border: none;
     background-color: rgba(255, 255, 255, 1);
   }
 
-  .ag-floating-bottom {
-    border-top: 1px solid var(--gray-4, #e4e6f0);
-    background: #fff;
+  .ag-center-cols-clipper {
+  }
+  // 헤더 배경
+  .ag-header-viewport {
+    background-color: ${theme.colors.gray6};
   }
 
+  .ag-header ag-pivot-off {
+  }
+
+  // 헤더 가운데 정렬
+  .ag-header-cell-label {
+    justify-content: center;
+    font-size: 14px;
+    font-family: Pretendard Bold;
+    line-height: 20px;
+    letter-spacing: -0.28px;
+  }
+
+  // 테이블 테두리 제거
   .ag-root-wrapper {
     border: none;
     border-radius: 12px;
   }
 
-  .ag-center-cols-container {
-    letter-spacing: -0.24px;
+  // 합계 테두리 변경
+  .ag-floating-bottom {
+    border-top: 1px solid ${theme.colors.gray4};
+    background: #fff;
+  }
+
+  // 헤더 테두리 변경
+  .ag-header {
+    border-bottom: 1px solid ${theme.colors.gray4};
+    line-height: 20px;
+    letter-spacing: -0.28px;
+  }
+
+  // 바디 로우 첫 번째 컬럼 폰트 변경
+  .ag-center-cols-container [aria-colindex='1'] {
+    font-family: Pretendard SemiBold;
     line-height: 16px;
+    letter-spacing: -0.24px;
+  }
+
+  // 합계 폰트 변경
+  .ag-floating-bottom-container .ag-cell {
+    font-size: 14px;
+    font-family: Pretendard SemiBold;
+    line-height: 16px;
+    letter-spacing: -0.28px;
+  }
+
+  // 합계 2,3,4 열 폰트 색상 변경
+  .ag-floating-bottom-container [aria-colindex='2'],
+  .ag-floating-bottom-container [aria-colindex='3'],
+  .ag-floating-bottom-container [aria-colindex='4'] {
+    color: ${theme.colors.blue};
   }
 `;
