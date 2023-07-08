3b728681a058e5c2a4ac5960cb9e70787af6ee5d
dihoon <dihoon955@gmail.com>
2023-07-09 02:09:17 +0900

    메뉴 드랍다운 추가

--- src/components/Navbar/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem.tsx
@@ -1,26 +1,70 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useCallback, useState } from 'react';
 import styled from '@emotion/styled';
+import ArrowDropUp from '../../../public/images/icons/arrow_drop_up.svg';
+import Ellipse from '../../../public/images/icons/ellipse.svg';
+import { Flex } from '@chakra-ui/react';
+import theme from '@/theme/theme';
+import { useRouter } from 'next/router';
 
-type Props = {
+export type SubMenu = {
+  title: string;
+  route: string;
+};
+
+export type Props = {
   title: string;
   children: ReactNode;
   onClick: () => void;
   isActive?: boolean;
+  subMenu?: SubMenu[];
 };
 
 const NavbarItem = (props: Props) => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const router = useRouter();
+
+  const handleMouseEnter = useCallback(() => {
+    setIsMouseOver(true);
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    setIsMouseOver(false);
+  }, []);
+
   return (
-    <MenuListItem onClick={props.onClick} isActive={props?.isActive}>
-      <ImageWrapper>{props.children}</ImageWrapper>
-      <MenuTitle>{props.title}</MenuTitle>
-    </MenuListItem>
+    <MenuListItemWrapper isActive={props?.isActive} onMouseEnter={() => handleMouseEnter()} onMouseLeave={() => handleMouseLeave()}>
+      <MenuListItem onClick={props.onClick} isActive={props?.isActive} isMouseOver={isMouseOver}>
+        <Flex>
+          <ImageWrapper>{props.children}</ImageWrapper>
+          <MenuTitle>{props.title}</MenuTitle>
+        </Flex>
+        {props.subMenu && <ArrowDropUp width="24px" height="24px" />}
+      </MenuListItem>
+      {(isMouseOver || props.isActive) && props.subMenu && (
+        <SubMenu>
+          <SubMenuItemWrapper>
+            {props?.subMenu.map((menu, index) => {
+              return (
+                <SubMenuItem key={index} onClick={() => router.push(menu.route)} isSelected={router.pathname.includes(menu.route)}>
+                  <Ellipse width="4px" height="4px" />
+                  {menu.title}
+                </SubMenuItem>
+              );
+            })}
+          </SubMenuItemWrapper>
+        </SubMenu>
+      )}
+    </MenuListItemWrapper>
   );
 };
 
 export default NavbarItem;
 
+const MenuListItemWrapper = styled.div<any>``;
+
 const MenuListItem = styled.li<any>`
   display: flex;
+  justify-content: space-between;
   align-items: center;
   height: 56px;
   margin-bottom: 16px;
@@ -36,13 +80,19 @@ const MenuListItem = styled.li<any>`
   &:hover {
     background-color: ${props => props.theme.colors.blue};
     color: ${props => props.theme.colors.white};
+    & > svg {
+      transform: rotate(180deg);
+    }
   }
 
   ${props =>
-    props.isActive &&
+    (props.isActive || props.isMouseOver) &&
     `
     background-color: ${props.theme.colors.blue};
     color: ${props.theme.colors.white};
+    & > svg {
+      transform: rotate(180deg);
+    }
   `}
 `;
 
@@ -58,4 +108,32 @@ const MenuTitle = styled.span`
   font-weight: 500;
   line-height: 24px;
   letter-spacing: -0.04em;
+`;
+
+const SubMenu = styled.li`
+  padding: 10px 16px;
+  color: ${theme.colors.gray3};
+`;
+
+const SubMenuItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const SubMenuItem = styled.div<any>`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  cursor: pointer;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -0.32px;
+  color: ${({ isSelected }) => (isSelected ? theme.colors.blue : theme.colors.gray3)};
+  font-family: ${({ isSelected }) => (isSelected ? 'Pretendard SemiBold' : 'Pretendard Regular')};
+
+  &:hover {
+    color: ${theme.colors.blue};
+    font-family: 'Pretendard SemiBold';
+  }
 `;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,34 @@
 import NavbarItem from './NavbarItem';
 import { useNavbar } from './useNavbar';
-import { ReactNode } from 'react';
+import type { SubMenu } from './NavbarItem';
+import { ReactNode, useEffect, useCallback } from 'react';
 import styled from '@emotion/styled';
 import Home from '../../../public/images/icons/home.svg';
 import EditSquare from '../../../public/images/icons/editSquare.svg';
 import School from '../../../public/images/icons/graduationOutline.svg';
 import Analytics from '../../../public/images/icons/analytics.svg';
 import Settings from '../../../public/images/icons/settings.svg';
+import UserManagement from '../../../public/images/icons/manageAccounts.svg';
 import { useRouter } from 'next/router';
 
+type Menu = {
+  icon: ReactNode;
+  title: string;
+  route: string;
+  subMenu: SubMenu[];
+};
+
 interface Props {
-  menuList: { icon: ReactNode; title: string; route: string }[];
+  menuList: Menu[];
 }
 
 export const Navbar = (props: Props) => {
   const { rootProps } = useNavbar();
   const router = useRouter();
+
+  const handleClick = useCallback((item: Menu) => {
+    if (!item.subMenu) router.push(item.route);
+  }, []);
 
   return (
     <NavWrapper>
@@ -23,7 +36,7 @@ export const Navbar = (props: Props) => {
       <MenuList>
         {props?.menuList?.map((item, index) => {
           return (
-            <NavbarItem key={index} title={item.title} onClick={() => router.push(item.route)} isActive={item.route.includes(router.pathname)}>
+            <NavbarItem key={index} title={item.title} onClick={() => handleClick(item)} isActive={router.pathname.startsWith(item.route)} subMenu={item?.subMenu}>
               {item.icon}
             </NavbarItem>
           );
@@ -45,6 +58,15 @@ Navbar.defaultProps = {
     { icon: <Analytics />, title: '합격분석', route: '/passAnalysis' },
     { icon: <Settings />, title: '환경설정', route: '/settings?menu=0' },
     { icon: <Analytics />, title: '사전예약', route: '/reservations' },
+    {
+      icon: <UserManagement />,
+      title: '사용자 관리',
+      route: '/userManagement',
+      subMenu: [
+        { title: '사용자 목록', route: '/userManagement/userList' },
+        { title: 'UPDATE 관리', route: '/userManagement/updateManagement' },
+      ],
+    },
   ],
 };
 
