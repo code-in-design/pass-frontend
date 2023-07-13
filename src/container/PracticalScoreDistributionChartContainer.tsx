879d2e8dd66601a6713de3abac76508504965521
seonsubin <stnqls3938@gmail.com>
2023-07-13 14:17:32 +0900

    수정

--- src/container/PracticalScoreDistributionChartContainer.tsx
+++ src/container/PracticalScoreDistributionChartContainer.tsx
@@ -9,30 +9,25 @@ interface Props {
   onClose: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+const practicals = ['제자리 멀리뛰기', '배근력', '사이드스텝', '메디신볼던지기', '매달리기'];
+const data = [
+  { men: '300이상', women: '250이상' },
+  { men: '297-299', women: '247-249' },
+  { men: '294-296', women: '244-246' },
+  { men: '291-293', women: '241-243' },
+  { men: '288-290', women: '238-240' },
+  { men: '300이상', women: '250이상' },
+  { men: '297-299', women: '247-249' },
+  { men: '294-296', women: '244-246' },
+  { men: '291-293', women: '241-243' },
+  { men: '288-290', women: '238-240' },
+  { men: '294-296', women: '244-246' },
+  { men: '291-293', women: '241-243' },
+  { men: '288-290', women: '238-240' },
+];
+
 const PracticalScoreDistributionChartContainer = (props: Props) => {
-  return <PracticalScoreDistributionModal name={props.name} subTitle={props.subTitle} onClose={props.onClose} exercise={props.practicals} data={props.data} />;
-};
-
-PracticalScoreDistributionChartContainer.defaultProps = {
-  practicals: ['제자리 멀리뛰기', '배근력', '사이드스텝', '메디신볼던지기', '매달리기'],
-  data: [
-    { men: '300이상', women: '250이상' },
-    { men: '297-299', women: '247-249' },
-    ,
-    { men: '294-296', women: '244-246' },
-    { men: '291-293', women: '241-243' },
-    { men: '288-290', women: '238-240' },
-    { men: '300이상', women: '250이상' },
-    { men: '297-299', women: '247-249' },
-    ,
-    { men: '294-296', women: '244-246' },
-    { men: '291-293', women: '241-243' },
-    { men: '288-290', women: '238-240' },
-
-    { men: '294-296', women: '244-246' },
-    { men: '291-293', women: '241-243' },
-    { men: '288-290', women: '238-240' },
-  ],
+  return <PracticalScoreDistributionModal name={props.name} subTitle={props.subTitle} onClose={props.onClose} exercise={practicals} data={data} />;
 };
 
 export default PracticalScoreDistributionChartContainer;
